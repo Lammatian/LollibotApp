c3003371357e622c51491ca71c3fac1e0e29ebd4
Rusab <rusab36@gmail.com>
2018-02-07 15:55:31 +0000

    * Changed Adapter to Custom adapter with some UI
    TODO:
    * Set Onclick to pass the MAC and Name of device as an Intent.
    * Maybe changed the color scheme?

--- app/src/main/java/com/alliedtech/lollibotapp/BluetoothScanner.java
+++ app/src/main/java/com/alliedtech/lollibotapp/BluetoothScanner.java
@@ -3,6 +3,7 @@ package com.alliedtech.lollibotapp;
 //Basically has methods to handle the bluetooth, and return all devices etc, seperate because easier to test I think
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -11,22 +12,26 @@ import android.content.IntentFilter;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
+import java.util.Set;
 
-public class BluetoothScanner {
+class BluetoothScanner {
     private BluetoothAdapter btAdapter;
     private boolean blueToothEnabled; //Boolean to check if bluetooth is enabled or not
-    private ArrayList<BluetoothDevice> scannedDevicesArraylist; //ArrayList to store all the devices
-    private ArrayList<String> devicesNameAndMac; //To be changed when custom adapter made, for now just stores devices name and MAC
-    private ArrayAdapter<String> scannedDevicesAdapter; //To be changed to a custom adapter for more customizability
+    private ArrayList<String> devicesNames;
+    private ArrayList<String> devicesMacs;
+    private ArrayList<Integer> deviceSignalStrength;
+    private boolean scanning;
     private Context context;
 
     BluetoothScanner(Context context) {
         this.context = context;
         btAdapter = BluetoothAdapter.getDefaultAdapter();
         blueToothEnabled = !(btAdapter == null || !(btAdapter.isEnabled()));
-        scannedDevicesArraylist = new ArrayList<>();
-        devicesNameAndMac = new ArrayList<>();
-        scannedDevicesAdapter = new ArrayAdapter<>(this.context, android.R.layout.simple_list_item_1, devicesNameAndMac);
+        devicesNames = new ArrayList<>();
+        devicesMacs = new ArrayList<>();
+        deviceSignalStrength = new ArrayList<>();
+
+        scan();
 
     }
 
@@ -36,34 +41,50 @@ public class BluetoothScanner {
     }
 
     //Method to return all devices scannable, whether bonded or not (Adapater has string with name and MAC address)
-    ArrayAdapter<String> allDevices() {
-        scan();
-        scannedDevicesAdapter = new ArrayAdapter<String>(this.context,android.R.layout.simple_list_item_1, devicesNameAndMac);
-        return scannedDevicesAdapter;
+    ArrayList<String> getDevicesNames() {
+        return devicesNames;
+    }
+    ArrayList<String> getDevicesMacs() {
+        return devicesMacs;
+    }
+    ArrayList<Integer> getDevicesSignals() {
+        return deviceSignalStrength;
     }
 
     //This is the receiver that basically returns all devices whether bonded or not
     private final BroadcastReceiver blReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-            int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE); //Signal Strength measured in dBm
-            // add the name to the list
-            scannedDevicesArraylist.add(device);
-            //Terrible way to add it, but for now just doing this (More negative RSSI means more strength)
-            devicesNameAndMac.add("Name: " + device.getName() + ", MAC address: " + device.getAddress() + ", Signal Strength : " + rssi);
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE); //Signal Strength measured in dBm
+                //(More positive RSSI means more strength)
+                if (!devicesMacs.contains(device.getAddress()) && (device.getName() != null)) {
+                    devicesNames.add(device.getName());
+                    devicesMacs.add(device.getAddress());
+                    deviceSignalStrength.add(rssi);
+                    DevicesListActivity.deviceListAdapter.notifyDataSetChanged();
+                }
+            }
+
         }
 
     };
 
     //Gets all bluetooth devices, and the blReceiver adds all the devices to the arraylist
     private void scan() {
+        Set<BluetoothDevice> btArrayList = btAdapter.getBondedDevices();
+
+        for (BluetoothDevice device : btArrayList) {
+            devicesNames.add(device.getName());
+            devicesMacs.add(device.getAddress());
+            deviceSignalStrength.add(100);
+        }
        btAdapter.startDiscovery();
        context.registerReceiver(blReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
 
-    }
-    public ArrayList getDevices() {
-        return this.scannedDevicesArraylist;
+
     }
 
 
--- app/src/main/java/com/alliedtech/lollibotapp/DevicesListActivity.java
+++ app/src/main/java/com/alliedtech/lollibotapp/DevicesListActivity.java
@@ -3,17 +3,23 @@ package com.alliedtech.lollibotapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-//Home Screen Activity, looks very basic atm but it
+import java.util.ArrayList;
+
+//Home Screen Activity, looks very basic atm but it'll be better
 
 public class DevicesListActivity extends AppCompatActivity {
 
-    ListView viewOfAllDevices;
+   public static ListView viewOfAllDevices;
     BluetoothScanner btScanner;
-    ArrayAdapter<String> adapterOfDevices;
+    public static DeviceListAdapter deviceListAdapter;
+    ArrayList<String> deviceNames;
+    ArrayList<String> deviceMacs;
+    ArrayList<Integer> deviceScans;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,24 +27,44 @@ public class DevicesListActivity extends AppCompatActivity {
 
         viewOfAllDevices = findViewById(R.id.devicesList);
         btScanner = new BluetoothScanner(this);
+        deviceNames = new ArrayList<>();
+        deviceMacs = new ArrayList<>();
+        deviceScans = new ArrayList<>();
 
+        deviceNames  = btScanner.getDevicesNames();
+        deviceMacs = btScanner.getDevicesMacs();
+        deviceScans = btScanner.getDevicesSignals();
         if (btScanner.isAvailable()) {
-            adapterOfDevices = btScanner.allDevices();
-            viewOfAllDevices.setAdapter(adapterOfDevices);
+            deviceListAdapter = new DeviceListAdapter(deviceNames,deviceMacs,deviceScans,this);
+            viewOfAllDevices.setAdapter(deviceListAdapter);
         }
         else {
             Toast.makeText(getApplicationContext(),"Bluetooth not available",Toast.LENGTH_SHORT).show();
         }
+
+        viewOfAllDevices.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Can pass MAC address as Intent, since thats mostly what we all need for the EV3, UUID's will be same for all EV3 (We'll set it up to be)
+            }
+        });
 
     }
     public void reScan(View v) {
         if (btScanner.isAvailable()) {
-            adapterOfDevices = btScanner.allDevices();
-            viewOfAllDevices.setAdapter(adapterOfDevices);
+            deviceNames.clear();
+            deviceMacs.clear();
+            deviceScans.clear();
+            deviceListAdapter.notifyDataSetChanged();
+            btScanner = new BluetoothScanner(getApplicationContext());
+            deviceNames = btScanner.getDevicesNames();
+            deviceMacs = btScanner.getDevicesMacs();
+            deviceScans = btScanner.getDevicesSignals();
+            deviceListAdapter = new DeviceListAdapter(deviceNames,deviceMacs,deviceScans,this);
+            viewOfAllDevices.setAdapter(deviceListAdapter);
         }
         else {
             Toast.makeText(getApplicationContext(),"Bluetooth not available",Toast.LENGTH_SHORT).show();
         }
-
     }
 }
