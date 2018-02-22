3ed78f6983643fec3781e299c461608e39913fe0
Lammatian <mateusz.parafinski@gmail.com>
2018-02-22 21:39:33 +0000

    Implemented setting date in DayScheduleFragment

--- app/src/main/java/com/alliedtech/lollibotapp/DailyScheduleFragment.java
+++ app/src/main/java/com/alliedtech/lollibotapp/DailyScheduleFragment.java
@@ -1,5 +1,6 @@
 package com.alliedtech.lollibotapp;
 
+import android.app.DatePickerDialog;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -8,36 +9,55 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+
 import com.alliedtech.lollibotapp.adapters.DailyScheduleAdapter;
 import com.alliedtech.lollibotapp.decoration.SpacesItemDecoration;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
-public class DailyScheduleFragment extends Fragment {
+public class DailyScheduleFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
 
     private View view;
+    private Button addDateButton;
     private Button addRunButton;
     private RecyclerView recyclerView;
     private ArrayList<Run> runs;
+    private DailyScheduleFragment fragment = this;
     private DailyScheduleAdapter dailyScheduleRecyclerAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.daily_schedule_fragment, container, false);
 
+        setAddDate();
         setGridView();
         setAddRunButton();
 
         return view;
     }
 
+    private void setAddDate() {
+        addDateButton = view.findViewById(R.id.addDateButton);
+        addDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+                        fragment,
+                        1970,
+                        1,
+                        1);
+                datePickerDialog.show();
+            }
+        });
+    }
+
     private void setAddRunButton() {
         addRunButton = view.findViewById(R.id.addRunButton);
-
-        // TODO: array hours gets cleared when adding new item
-        // TODO: that's because we're not actually adding date to the array
         addRunButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,5 +81,15 @@ public class DailyScheduleFragment extends Fragment {
         recyclerView.setHasFixedSize(false);
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
         recyclerView.addItemDecoration(new SpacesItemDecoration(1));
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        Calendar date = Calendar.getInstance();
+        date.set(Calendar.YEAR, year);
+        date.set(Calendar.MONTH, month);
+        date.set(Calendar.DAY_OF_MONTH, day);
+        addDateButton.setText(new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                .format(date.getTime()));
     }
 }
