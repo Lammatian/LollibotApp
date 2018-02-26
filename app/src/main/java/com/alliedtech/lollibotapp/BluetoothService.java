8e0eb2f6227c538e175b21cd5477fe4c23446d77
Lammatian <mateusz.parafinski@gmail.com>
2018-02-26 15:46:44 +0000

    Refactoring, changed connection handling

--- app/src/main/java/com/alliedtech/lollibotapp/BluetoothService.java
+++ app/src/main/java/com/alliedtech/lollibotapp/BluetoothService.java
@@ -2,7 +2,6 @@ package com.alliedtech.lollibotapp;
 
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
@@ -30,10 +29,12 @@ public class BluetoothService extends Service {
     private static final String TAG = "BLUETOOTH SERVICE";
     private Handler mHandler; // handler that gets info from Bluetooth service
     private BluetoothAdapter btAdapter;
+    private Set<BluetoothDevice> bondedDevices;
+    private Set<BluetoothDevice> discoveredDevices;
     private ArrayList<String> devicesNames;
     private ArrayList<String> devicesMacs;
     private ArrayList<Integer> deviceSignalStrength;
-    private Context context;
+    private Context context = getApplicationContext();
     private final LocalBinder binder = new LocalBinder();
 
     @Nullable
@@ -94,9 +95,9 @@ public class BluetoothService extends Service {
 
     //Gets all bluetooth devices, and the btReceiver adds all the devices to the arraylist
     private void scan() {
-        Set<BluetoothDevice> btArrayList = btAdapter.getBondedDevices();
+        bondedDevices = btAdapter.getBondedDevices();
 
-        for (BluetoothDevice device : btArrayList) {
+        for (BluetoothDevice device : bondedDevices) {
             devicesNames.add(device.getName());
             devicesMacs.add(device.getAddress());
             deviceSignalStrength.add(100);
@@ -106,23 +107,22 @@ public class BluetoothService extends Service {
         context.registerReceiver(btReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
     }
 
-    // Defines several constants used when transmitting messages between the
-    // service and the UI.
-    private interface MessageConstants {
-        public static final int MESSAGE_READ = 0;
-        public static final int MESSAGE_WRITE = 1;
-        public static final int MESSAGE_TOAST = 2;
-
-        // ... (Add other message types here as needed.)
+    public void connect(BluetoothDevice device) {
+        ConnectThread connectThread = new ConnectThread(device);
+        connectThread.start();
     }
 
-    //region Connection thread
-    private class ConnectedThread extends Thread {
+    public void connected(BluetoothSocket socket) {
+        ConnectedThread connectedThread = new ConnectedThread(socket);
+        connectedThread.start();
+    }
+
+    //region Establishing connection thread
+    private class ConnectThread extends Thread {
         private final BluetoothSocket mmSocket;
         private final BluetoothDevice mmDevice;
-        private byte[] btBuff;
 
-        public ConnectedThread(BluetoothDevice device) {
+        public ConnectThread(BluetoothDevice device) {
             // Use a temporary object that is later assigned to mmSocket
             // because mmSocket is final.
             BluetoothSocket tmp = null;
@@ -157,20 +157,7 @@ public class BluetoothService extends Service {
                 return;
             }
 
-            // The connection attempt succeeded. Perform work associated with
-            // the connection in a separate thread.
-//            manageMyConnectedSocket(mmSocket);
-        }
-
-        public void write(byte[] buffer) {
-            try {
-                mmSocket.getOutputStream().write(buffer);
-
-                // Share the sent message back to the UI Activity
-//                mHandler.obtainMessage(AbstractActivity.MESSAGE_WRITE, buffer.length, -1, buffer).sendToTarget();
-            } catch (IOException e) {
-                Log.e("PrinterService", "Exception during write", e);
-            }
+            connected(mmSocket);
         }
 
         // Closes the client socket and causes the thread to finish.
@@ -179,6 +166,100 @@ public class BluetoothService extends Service {
                 mmSocket.close();
             } catch (IOException e) {
                 Log.e(TAG, "Could not close the client socket", e);
+            }
+        }
+    }
+    //endregion
+
+    //region Managing connection thread
+    // Defines several constants used when transmitting messages between the
+    // service and the UI.
+    private interface MessageConstants {
+        public static final int MESSAGE_READ = 0;
+        public static final int MESSAGE_WRITE = 1;
+        public static final int MESSAGE_TOAST = 2;
+
+        // ... (Add other message types here as needed.)
+    }
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+        private byte[] mmBuffer; // mmBuffer store for the stream
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams; using temp objects because
+            // member streams are final.
+            try {
+                tmpIn = socket.getInputStream();
+            } catch (IOException e) {
+                Log.e(TAG, "Error occurred when creating input stream", e);
+            }
+            try {
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                Log.e(TAG, "Error occurred when creating output stream", e);
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            mmBuffer = new byte[1024];
+            int numBytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs.
+            while (true) {
+                try {
+                    // Read from the InputStream.
+                    numBytes = mmInStream.read(mmBuffer);
+                    // Send the obtained bytes to the UI activity.
+                    Message readMsg = mHandler.obtainMessage(
+                            MessageConstants.MESSAGE_READ, numBytes, -1,
+                            mmBuffer);
+                    readMsg.sendToTarget();
+                } catch (IOException e) {
+                    Log.d(TAG, "Input stream was disconnected", e);
+                    break;
+                }
+            }
+        }
+
+        // Call this from the main activity to send data to the remote device.
+        public void write(byte[] bytes) {
+            try {
+                mmOutStream.write(bytes);
+
+                // Share the sent message with the UI activity.
+                Message writtenMsg = mHandler.obtainMessage(
+                        MessageConstants.MESSAGE_WRITE, -1, -1, mmBuffer);
+                writtenMsg.sendToTarget();
+            } catch (IOException e) {
+                Log.e(TAG, "Error occurred when sending data", e);
+
+                // Send a failure message back to the activity.
+                Message writeErrorMsg =
+                        mHandler.obtainMessage(MessageConstants.MESSAGE_TOAST);
+                Bundle bundle = new Bundle();
+                bundle.putString("toast",
+                        "Couldn't send data to the other device");
+                writeErrorMsg.setData(bundle);
+                mHandler.sendMessage(writeErrorMsg);
+            }
+        }
+
+        // Call this method from the main activity to shut down the connection.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the connect socket", e);
             }
         }
     }
